e7fe5980e18918870375ba1821e9f3bd3189f2ff
aurianeb <auriane.blarre@eleves.enpc.fr>
2017-10-03 23:20:47 -0700

    Finished homework 2

--- java/src/AggressiveDriver.java
+++ java/src/AggressiveDriver.java
@@ -5,8 +5,17 @@ public class AggressiveDriver extends Driver {
     }
 
     @Override
+    public double throttle_action(Car car, Environment env) {
+
+        if(car.current_speed<car.max_speed)
+            return car.max_acceleration;
+        else
+            return -car.max_acceleration;
+    }
+
     public boolean stop_for_refueling(Car car, Environment env) {
         return false;
     }
 
 }
+
